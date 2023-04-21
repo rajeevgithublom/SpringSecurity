18da57f4beff585ee2090306e64c6ea4c0c43def
Rajeev Singh <rjvsingh88@gmail.com>
2023-04-21 15:08:39 +0530

    changes in main method

--- 01SpringBootSecurityInMemoryAuthEx/src/main/java/com/app/raghu/SpringBootSecurityInMemoryAuthExApplication.java
+++ 01SpringBootSecurityInMemoryAuthEx/src/main/java/com/app/raghu/SpringBootSecurityInMemoryAuthExApplication.java
@@ -9,6 +9,8 @@ public class SpringBootSecurityInMemoryAuthExApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityInMemoryAuthExApplication.class, args);
 	    System.out.println("added a git message");
+	    System.out.println("added another line in github ");
+	    
 	
 	}
 
